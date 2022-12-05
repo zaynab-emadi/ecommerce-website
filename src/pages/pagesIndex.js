92b80ae59d74f06b0edb634f6fdedb6836c7fb57
zaynab18 <zaynabemadi@gmail.com>
2022-12-05 16:23:04 +0330

    add new page

--- src/pages/pagesIndex.js
+++ src/pages/pagesIndex.js
@@ -1,8 +1,8 @@
-export {AdminLogin} from "./admin panel/adminLogin/adminLogin";
-export {Dashboard} from "./admin panel/dashboard/dashboard";
-export {Orders} from "./admin panel/orders/orders";
-export {ProductsList} from "./admin panel/productsList/productsList";
-export {Stock} from "./admin panel/stock/stock";
+export {AdminLogin} from "./adminpanel/adminLogin/adminLogin";
+export {Dashboard} from "./adminpanel/dashboard/dashboard";
+export {Orders} from "./adminpanel/orders/orders";
+export {ProductsList} from "./adminpanel/productsList/productsList";
+export {Stock} from "./adminpanel/stock/stock";
 export {Cart} from "./shop/cart/cart";
 export {Category} from "./shop/category/category";
 export {CustomerInfo } from "./shop/customerInfo/customerInfo";
@@ -10,3 +10,4 @@ export {IntroPage} from "./shop/introPage/introPage";
 export {OrderSubmit} from "./shop/orderSubmit/orderSubmit";
 export {PaymentResult} from "./shop/paymentResult/paymentResult";
 export {ProductDetail} from "./shop/productDetail/productDetail";
+export {AdminPanel} from "./adminpanel/adminPanel/adminPanel";
--- src/routes/routesIndex.js
+++ src/routes/routesIndex.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import {BrowserRouter , Routes , Route} from "react-router-dom";
+import {AdminLogin, Dashboard, IntroPage} from "../pages/pagesIndex";
 
 function RoutesIndex() {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route ></Route>
+                    <Route path="/" element={<IntroPage/>}>
+
+                    </Route>
+                    <Route path="/loginAdmin" element={<AdminLogin/>}/>
+                    <Route path="/adminPanel/dashboard" element={<Dashboard/>} />
                 </Routes>
             </BrowserRouter>
         </>
